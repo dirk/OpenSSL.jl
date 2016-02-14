6f10beb6c2cfd21188b17785c0017600f488f24b
Hatsune Miku <999Hatsune@gmail.com>
2016-02-14 09:53:34 +0900

    add module OpenSSL.Cipher

--- src/OpenSSL.jl
+++ src/OpenSSL.jl
@@ -5,6 +5,18 @@ module OpenSSL
 
   const LIBCRYPTO = ENV["OS"] == "Windows_NT" ? "libeay32" : "libcrypto"
 
+  function init()
+    # ccall((:OpenSSL_add_all_digests, OpenSSL.LIBCRYPTO), Void, ())
+    # ccall((:OpenSSL_add_all_ciphers, OpenSSL.LIBCRYPTO), Void, ())
+    # ccall((:OPENSSL_add_all_algorithms_conf, OpenSSL.LIBCRYPTO), Void, ())
+    ccall((:OPENSSL_add_all_algorithms_noconf, OpenSSL.LIBCRYPTO), Void, ())
+    # alias OpenSSL_add_all_algorithms :OPENSSL_add_all_algorithms_noconf
+  end
+
+  function cleanup()
+    ccall((:EVP_cleanup, OpenSSL.LIBCRYPTO), Void, ())
+  end
+
   module Digest
     import OpenSSL
 
@@ -20,7 +32,7 @@ module OpenSSL
       ctx = ccall((:EVP_MD_CTX_create, OpenSSL.LIBCRYPTO), Ptr{Void}, ())
       try
         # Get the message digest struct
-        md = ccall((:EVP_get_digestbyname, OpenSSL.LIBCRYPTO), Ptr{Void}, (Ptr{UInt8},), bytestring(name))
+        md = ccall((:EVP_get_digestbyname, OpenSSL.LIBCRYPTO), Ptr{Void}, (Ptr{UInt8},), name)
         if(md == C_NULL)
           error("Unknown message digest $name")
         end
@@ -43,7 +55,7 @@ module OpenSSL
       ctx = ccall((:EVP_MD_CTX_create, OpenSSL.LIBCRYPTO), Ptr{Void}, ())
       try
         # Get the message digest struct
-        md = ccall((:EVP_get_digestbyname, OpenSSL.LIBCRYPTO), Ptr{Void}, (Ptr{UInt8},), bytestring(name))
+        md = ccall((:EVP_get_digestbyname, OpenSSL.LIBCRYPTO), Ptr{Void}, (Ptr{UInt8},), name)
         if(md == C_NULL)
           error("Unknown message digest $name")
         end
@@ -86,5 +98,62 @@ module OpenSSL
     end#/digestfinalize
 
   end#/Digest
+
+  module Cipher
+    import OpenSSL
+
+    function init()
+      ccall((:OpenSSL_add_all_ciphers, OpenSSL.LIBCRYPTO), Void, ())
+    end
+
+    function cleanup()
+      ccall((:EVP_cleanup, OpenSSL.LIBCRYPTO), Void, ())
+    end
+
+    function encrypt(name::AbstractString, key::Array{UInt8,1}, iv::Array{UInt8,1}, plain::Array{UInt8,1})
+      ctx = ccall((:EVP_CIPHER_CTX_new, OpenSSL.LIBCRYPTO), Ptr{Void}, ())
+      ccall((:EVP_CIPHER_CTX_init, OpenSSL.LIBCRYPTO), Void, (Ptr{Void},), ctx)
+      try
+        enc = Array{UInt8,1}([])
+        # ec = ccall((:EVP_get_cipherbyname, OpenSSL.LIBCRYPTO), Ptr{Void}, (Ptr{UInt8},), name)
+        algorithm = Symbol("EVP_"*name)
+        if(algorithm != :EVP_aes_256_cbc)
+          error("Not support cipher algorithm $name")
+        end
+        ec = ccall((:EVP_aes_256_cbc, OpenSSL.LIBCRYPTO), Ptr{Void}, ())
+        if(ec == C_NULL)
+          error("Unknown cipher algorithm $name")
+        end
+        ccall((:EVP_EncryptInit_ex, OpenSSL.LIBCRYPTO), Void, (Ptr{Void}, Ptr{Void}, Ptr{Void}, Ptr{UInt8}, Ptr{UInt8}), ctx, ec, C_NULL, key, iv)
+
+        tmpenc = Array{UInt8,1}([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+        tmplen = UInt(0)
+        blksize = ccall((:EVP_CIPHER_CTX_block_size, OpenSSL.LIBCRYPTO), UInt, (Ptr{Void},), ctx)
+
+        ccall((:EVP_EncryptUpdate, OpenSSL.LIBCRYPTO), Void, (Ptr{Void}, Ptr{UInt8}, Ptr{UInt}, Ptr{UInt8}, UInt), ctx, tmpenc, &tmplen, plain, blksize)
+        enc = [enc; tmpenc]
+
+        remain = 0
+        if(length(plain) > 16) # if(remain > 0)
+          ccall((:EVP_EncryptFinal_ex, OpenSSL.LIBCRYPTO), Void, (Ptr{Void}, Ptr{UInt8}, Ptr{UInt}), ctx, tmpenc, &tmplen)
+          enc = [enc; tmpenc]
+        end
+
+        ccall((:EVP_CIPHER_CTX_cleanup, OpenSSL.LIBCRYPTO), Void, (Ptr{Void},), ctx)
+        return enc
+      finally
+        ccall((:EVP_CIPHER_CTX_free, OpenSSL.LIBCRYPTO), Void, (Ptr{Void},), ctx)
+      end
+    end#/encrypt
+
+    function decrypt(name::AbstractString, key::Array{UInt8,1}, iv::Array{UInt8,1}, bs::Array{UInt8,1})
+      ctx = ccall((:EVP_CIPHER_CTX_create, OpenSSL.LIBCRYPTO), Ptr{Void}, ())
+      try
+      finally
+        ccall((:EVP_CIPHER_CTX_destroy, OpenSSL.LIBCRYPTO), Void, (Ptr{Void},), ctx)
+      end
+    end#/decrypt
+
+  end#/Cipher
 
 end#/OpenSSL
--- test/test.jl
+++ test/test.jl
@@ -8,7 +8,7 @@ function string2bytes(s::AbstractString)
 end
 
 @test isdefined(:OpenSSL) == true
-OpenSSL.Digest.init()
+OpenSSL.init()
 
 s = OpenSSL.Digest.digest("SHA512", string2bytes("test"))
 println(s)
@@ -23,5 +23,19 @@ h = OpenSSL.Digest.digest("MD5",
 println(h)
 @test h == md5_of_bytes
 
-OpenSSL.Digest.cleanup()
+es = OpenSSL.Cipher.encrypt("aes_256_cbc",
+  hex2bytes("e299ff9d8e4831f07e5323913c53e5f0fec3a040a211d6562fa47607244d0051"),
+  hex2bytes("7c7ed9434ddb9c2d1e1fcc38b4bf4667"),
+  string2bytes("Message\t\t\t\t\t\t\t\t\t"))
+println(es)
+@test es == aes256cbc_of_shortdata
+
+el = OpenSSL.Cipher.encrypt("aes_256_cbc",
+  hex2bytes("e299ff9d8e4831f07e5323913c53e5f0fec3a040a211d6562fa47607244d0051"),
+  hex2bytes("7c7ed9434ddb9c2d1e1fcc38b4bf4667"),
+  string2bytes("Message\t\t\t\t\t\t\t\t\t\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10\x10"))
+println(el)
+@test el == aes256cbc_of_longdata
+
+OpenSSL.cleanup()
 println("All tests passed")
